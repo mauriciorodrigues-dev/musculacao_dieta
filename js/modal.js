140ccb2d59fb16acb4f4bc406fda2fb213768240
Mauricio Rodrigues <143841940+mauriciorodrigues-dev@users.noreply.github.com>
2023-10-18 18:32:15 -0300

    atc

---
+++ js/modal.js
@@ -0,0 +1,104 @@
+
+// Card UM
+const buttonModalUm = document.getElementById("modal-1");
+const modalUm = document.getElementById("desenv-musc");
+const fecharModal = document.getElementById("closeModal");
+
+buttonModalUm.onclick = function() {
+    modalUm.showModal();
+}
+
+fecharModal.onclick=function(){
+    modalUm.close()
+}
+
+// Card Dois
+const buttonModalDois = document.getElementById("modal-2");
+const modalDois = document.getElementById("perda-peso");
+const fecharModalDois = document.getElementById("closeModalDois");
+
+buttonModalDois.onclick = function() {
+    modalDois.showModal();
+}
+
+fecharModalDois.onclick=function(){
+    modalDois.close()
+}
+
+// Card TRES
+const buttonModalTres = document.getElementById("modal-3");
+const modalTres = document.getElementById("preven-lesao");
+const fecharModalTres = document.getElementById("closeModalTres");
+
+buttonModalTres.onclick = function() {
+    modalTres.showModal();
+}
+
+fecharModalTres.onclick=function(){
+    modalTres.close()
+}
+
+// Card Quatro
+const buttonModalQuatro = document.getElementById("modal-4");
+const modalQuatro = document.getElementById("saude-ossea");
+const fecharModalQuatro = document.getElementById("closeModalQuatro");
+
+buttonModalQuatro.onclick = function() {
+    modalQuatro.showModal();
+}
+
+fecharModalQuatro.onclick=function(){
+    modalQuatro.close()
+}
+
+// Card Cinco
+const buttonModalCinco = document.getElementById("modal-5");
+const modalCinco = document.getElementById("equil-horm");
+const fecharModalCinco = document.getElementById("closeModalCinco");
+
+buttonModalCinco.onclick = function() {
+    modalCinco.showModal();
+}
+
+fecharModalCinco.onclick=function(){
+    modalCinco.close()
+}
+
+// Card Seis
+const buttonModalSeis = document.getElementById("modal-6");
+const modalSeis = document.getElementById("saude-cardiov");
+const fecharModalSeis = document.getElementById("closeModalSeis");
+
+buttonModalSeis.onclick = function() {
+    modalSeis.showModal();
+}
+
+fecharModalSeis.onclick=function(){
+    modalSeis.close()
+}
+
+// Card Seis
+const buttonModalSete = document.getElementById("modal-7");
+const modalSete = document.getElementById("saude-mental");
+const fecharModalSete = document.getElementById("closeModalSete");
+
+buttonModalSete.onclick = function() {
+    modalSete.showModal();
+}
+
+fecharModalSete.onclick=function(){
+    modalSete.close()
+}
+
+// Card Seis
+const buttonModalOito = document.getElementById("modal-8");
+const modalOito = document.getElementById("melhor-post");
+const fecharModalOito = document.getElementById("closeModalOito");
+
+buttonModalOito.onclick = function() {
+    modalOito.showModal();
+}
+
+fecharModalOito.onclick=function(){
+    modalOito.close()
+}
